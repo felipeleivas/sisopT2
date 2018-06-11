99735f0c0bf8bf564ca1710fcff1491453be8aa9
Vinícius Scheffel <brsufirefox@gmail.com>
2018-06-11 15:33:28 -0300

    test_Write increased

--- teste/test_Write.c
+++ teste/test_Write.c
@@ -22,12 +22,13 @@
 int countFreeBlocks();
 
 int main() {
+
     int bufferSize = 1024 * 1024;
     char auxBuffer2[1024 * 1024] = {0};
 
     int testSuccess = TRUE;
     int i;
-    printf("-----------------------\n");
+    /* printf("-----------------------\n");
     int numOfFreeBlocksStart= (countFreeBlocks());
     // printf("There is %d free blocks\n",countFreeBlocks());
     FILE2 f = open2("file3");
@@ -226,11 +227,51 @@ int main() {
      }
 
      printAllStuff();
-     
+
     if (testSuccess == TRUE)
         printf("CONGRATZ THE TEST PASSED\n");
     else
-        printf("CONGRATZ THE YOU BREAK THE CODE\n");
+        printf("CONGRATZ THE YOU BREAK THE CODE\n"); */
+
+
+        printf("\n__________ PRINTANDO A SITUACAO ATUAL DO SISTEMA _________\n");
+        printAllStuff();
+
+        printf("\n\n________ CRIANDO A FILE teste no diretorio dir1, caminho absoluto __________");
+        create2("/dir1/teste");
+        printAllStuff();
+
+        printf("\n\n________ CRIANDO A FILE teste no diretorio dir1, NAO DEVE CRIAR __________");
+        create2("/dir1/teste");
+        printAllStuff();
+
+        printf("\n\n________ CRIANDO O DIRETORIO dirnovo no diretorio dir1, caminho absoluto ___________");
+        mkdir2("/dir1/dirnovo");
+        printAllStuff();
+
+        printf("\n\n________ CRIANDO O DIRETORIO dirnovo no diretorio dir1, caminho absoluto, NAO DEVE CRIAR ___________");
+        mkdir2("/dir1/dirnovo");
+        printAllStuff();
+
+        printf("\n\n________ CRIANDO O DIRETORIO dirnovodois no diretorio raiz, caminho absoluto ___________");
+        mkdir2("/dirnovodois");
+        printAllStuff();
+
+        printf("\n\n________ CRIANDO A FILE filepradeletar no diretorio dirnovo, caminho absoluto ___________");
+        create2("/dir1/dirnovo/filepradeletar");
+        printAllStuff();
+
+        printf("\n\n________ TENTANDO DELETAR O DIRETORIO dirnovo, NAO DEVE DELETAR! ___________");
+        rmdir2("/dir1/dirnovo");
+        printAllStuff();
+
+        printf("\n\n________ TENTANDO DELETAR A FILE filepradeletar, DEVE DELETAR! ___________");
+        delete2("/dir1/dirnovo/filepradeletar");
+        printAllStuff();
+
+        printf("\n\n________ TENTANDO DELETAR O DIRETORIO dirnovo, DEVE DELETAR! ___________");
+        rmdir2("/dir1/dirnovo");
+        printAllStuff();
 
     return 0;
 }
